c003a240c053f26da45633503d27de15e401d850
hyunjo01 <hyunjo10217@gmail.com>
2024-07-01 16:10:53 +0900

    FEAT: place dto

---
+++ course-web/src/main/java/capstone/courseweb/random/dto/ToSearchByKeywordDto.java
@@ -0,0 +1,8 @@
+package capstone.courseweb.random.dto;
+
+public class ToSearchByKeywordDto {
+    private String keyword;
+    private String x;
+    private String y;
+    private String radius;
+}
