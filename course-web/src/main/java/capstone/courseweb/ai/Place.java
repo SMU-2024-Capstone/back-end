6015efdda0acdc53acff9c57cc6a530359223391
hyunjo01 <hyunjo10217@gmail.com>
2024-09-11 12:39:58 +0900

    FEAT: 카테고리 검색 랜덤, POST 방식을 변경

--- course-web/src/main/java/capstone/courseweb/ai/Place.java
+++ course-web/src/main/java/capstone/courseweb/ai/Place.java
@@ -1,10 +1,10 @@
-/*package capstone.courseweb.ai;
+package capstone.courseweb.ai;
 
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "places")
-public class PlaceEntity {
+@Table(name = "PLACE")
+public class AiPlaceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer naver_id;
@@ -15,4 +15,4 @@ public class PlaceEntity {
     private String tag;
     private String address;
 
-}*/
+}
