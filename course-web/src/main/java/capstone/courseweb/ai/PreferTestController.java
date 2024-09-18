55ce19b7811b3fa73bc6c92268e0e496886dc21e
hyunjo01 <hyunjo10217@gmail.com>
2024-09-18 18:11:01 +0900

    FEAT: test-result 추천 장소 response에 rating 추가

--- course-web/src/main/java/capstone/courseweb/ai/PreferTestController.java
+++ course-web/src/main/java/capstone/courseweb/ai/PreferTestController.java
@@ -34,6 +34,7 @@ public class PreferTestController {
 
     @PostMapping("/test-result")
     public ResponseEntity<Map<String, List<Object>>> receiveTestResult(@RequestBody Map<String, Object> testResult) { //, @RequestHeader("Authorization")String token
+
         //jwt 토큰 검증
        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
@@ -45,6 +46,8 @@ public class PreferTestController {
         String nickname = authentication.getName(); // 사용자의 id 가져오기 (JwtAuthProvider에서 사용자 ID를 subject로 저장한 경우)
         log.info("jwt 토큰 검증 받은 사용자 id: {}",  nickname);
 
+
+        // String nickname = "현조"; // test 완료
         Optional<Member> memberOpt = memberRepository.findByNickname(nickname);
         if (memberOpt.isEmpty()) {
             Map<String, List<Object>> errorResponse = new HashMap<>();
@@ -81,11 +84,14 @@ public class PreferTestController {
             Optional<Place> place = placeRepository.findById(intArray[i]);
 
             if (place.isPresent()) {
+                int rating = placeService.getRatingForPlace(place.get().getId(), user.getId());
+
                 Map<String, Object> newResponse = new HashMap<>();
                 newResponse.put("placename", place.get().getName());
                 newResponse.put("category", place.get().getCategory());
                 newResponse.put("tag", place.get().getTag());
                 newResponse.put("URL", "https://pcmap.place.naver.com/restaurant/"+place.get().getId());
+                newResponse.put("rating", rating);
                 places_info.add(newResponse);
                 System.out.println(newResponse);
             }
@@ -98,7 +104,7 @@ public class PreferTestController {
 
 
     @PostMapping("/home/ai")
-    public ResponseEntity<Map<String, List<Object>>> receiveAiPlaces() { //선호도 테스트 다시 할 때는 test/result로 받아야 함.
+    public ResponseEntity<Map<String, List<Object>>> sendAiPlaces() { //선호도 테스트 다시 할 때는 test/result로 받아야 함.
 
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
